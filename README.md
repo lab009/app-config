edc1646a2696c94f5dbff3ad92ceb1acd98391f2
Oleg Orlov <mail@orlovoleg.com>
2016-03-13 00:07:06 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# app-config
+
+> Cascading configuration for web application
+
+### Installation Package
+
+```
+npm install lodash.merge 
+```
+
+### Usage
+
+For working in browser add AppConfigPlugin in webpack config.
+```js
+var webpackConfig = {
+  plugins: [
+    new AppConfigPlugin(),
+  ]
+}
+```
+
+### Package
+
+* lodash.merge
---
+++ webpack.js
@@ -0,0 +1,22 @@
+var webpack = require("webpack");
+var config = require("app-config");
+
+function AppConfigPlugin() {}
+module.exports = AppConfigPlugin;
+
+AppConfigPlugin.prototype.apply = function(compiler) {
+
+  compiler.apply(new webpack.DefinePlugin({
+    __CONFIG__: JSON.stringify(config)
+  }));
+
+  compiler.plugin("normal-module-factory", function(nmf) {
+    nmf.plugin("before-resolve", function(result, callback) {
+      if(!result) return callback();
+      if(result.request === "app-config") {
+        result.request = "app-config/client";
+      }
+      return callback(null, result);
+    });
+  });
+};
