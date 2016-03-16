c1aa3ee092ea9ab31ea379c54e781dbc2582cdf4
Oleg Orlov <mail@orlovoleg.com>
2016-03-16 12:54:36 +0300

    fix error with webpack plugin

--- webpack.js
+++ webpack.js
@@ -1,20 +1,19 @@
-var webpack = require("webpack");
-var config = require("app-config");
+var webpack = require('webpack');
 
 function AppConfigPlugin() {}
 module.exports = AppConfigPlugin;
 
 AppConfigPlugin.prototype.apply = function(compiler) {
 
-  compiler.apply(new webpack.DefinePlugin({
-    __CONFIG__: JSON.stringify(config)
-  }));
-
-  compiler.plugin("normal-module-factory", function(nmf) {
-    nmf.plugin("before-resolve", function(result, callback) {
+  compiler.plugin('normal-module-factory', function(nmf) {
+    nmf.plugin('before-resolve', function(result, callback) {
       if(!result) return callback();
-      if(result.request === "app-config") {
-        result.request = "app-config/lib/client";
+      if(/app-config$/.test(result.request)) {
+        compiler.apply(new webpack.DefinePlugin({
+          __CONFIG__: JSON.stringify(require(result.request))
+        }));
+
+        result.request += '/lib/client';
       }
       return callback(null, result);
     });
