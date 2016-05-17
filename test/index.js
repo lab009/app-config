361171c8ee1ede0734c7774498300651a1057e44
Oleg Orlov <mail@orlovoleg.com>
2016-05-17 15:09:47 +0300

    add test

---
+++ test/index.js
@@ -0,0 +1,57 @@
+import test from 'ava'
+import path from 'path'
+
+test.beforeEach(() => {
+  process.env.NODE_ENV = ''
+  const resolved = require.resolve('../')
+  delete require.cache[resolved]
+})
+
+test('should return empty when no config resolved', t => {
+  process.env.CONFIG_DIR = path.join(__dirname, 'doesnt-exist')
+  const config = require('../')
+
+  t.truthy(config)
+  t.deepEqual(config, {})
+})
+
+test('should work with provided config dir', t => {
+  process.env.CONFIG_DIR = path.join(__dirname, 'config')
+  const config = require('../')
+
+  t.truthy(config)
+})
+
+test('should load default when no env provided', t => {
+  process.env.CONFIG_DIR = path.join(__dirname, 'config')
+  const config = require('../')
+
+  t.truthy(config)
+  t.deepEqual(config, {
+    a: 3,
+    b: 2,
+    c: 1
+  })
+})
+
+test('should load env when provided', t => {
+  process.env.CONFIG_DIR = path.join(__dirname, 'config')
+  process.env.NODE_ENV = 'production'
+  const config = require('../')
+
+  t.truthy(config)
+  t.deepEqual(config, {
+    a: 1,
+    b: 2,
+    c: 3
+  })
+})
+
+test('should load env when provided', t => {
+  process.env.CONFIG_DIR = path.join(__dirname, 'config')
+  process.env.NODE_ENV = 'error'
+
+  t.throws(() => {
+    require('../')
+  }, /SyntaxError: /)
+})
