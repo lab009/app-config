cde00aa72c09fbec2a55fdadb7e772bcdbd09917
Oleg Orlov <mail@orlovoleg.com>
2016-05-30 17:19:11 +0300

    build

--- dist/client.js
+++ dist/client.js
@@ -1,8 +1,6 @@
 "use strict";
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
+exports.__esModule = true;
 // resolve object in webpack
 exports.default = __CONFIG__;
 module.exports = exports["default"];
